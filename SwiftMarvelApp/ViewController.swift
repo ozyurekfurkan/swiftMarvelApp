a6c0f9093110ff834d8f2abe397c529ecfaf1fc6
Furkan Özyürek <furkan.ozyurek@halkbank.com.tr>
2022-01-16 16:59:56 +0300

    Initial Commit

---
+++ SwiftMarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftMarvelApp
+//
+//  Created by Furkan Özyürek on 16.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
